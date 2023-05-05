ffe296e40f1489fc8e1dab9a80842ce550849307
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-05-05 18:31:22 +0530

    Added user input for changing grid size

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const container = document.querySelector(".container");
+const gridInput = document.querySelector("#gridInput");
+const gridInputSubmitButton = document.querySelector("button");
+
+let gridSize = 16;
+
+let clearGrid = () => {
+	container.innerText = "";
+}
 
 let createDivs = (n) => {
 	let gridColumns = [];
@@ -6,19 +14,31 @@ let createDivs = (n) => {
 		const div = document.createElement("div");
 		container.appendChild(div);
 		div.classList.add("child");
-}
+	}
 	for(let i = 0; i<n; i++){
 		gridColumns.push("1fr ");
 	}
-container.style.gridTemplateColumns = gridColumns.join("");
-container.style.gridTemplateRows = gridColumns.join("");
+	container.style.gridTemplateColumns = gridColumns.join("");
+	container.style.gridTemplateRows = gridColumns.join("");
+
+	const gridChildren = document.querySelectorAll(".child");
+
+	gridChildren.forEach((gridChild) => {
+		gridChild.addEventListener("mouseover", () => {
+			gridChild.style.backgroundColor = 'blue';
+		});
+	})
+
 }
 
-createDivs(64);
-const divs = document.querySelectorAll(".child");
+let getInputValue = () => {
+	gridSize = gridInput.value;
+	if (gridSize > 100) { 
+		gridSize = 100;
+	}
+	clearGrid();
+	createDivs(gridSize);
+}
 
-divs.forEach((div) => {
-	div.addEventListener("mouseover", () => {
-		div.style.backgroundColor = 'blue';
-	});
-})
+gridInputSubmitButton.addEventListener("click", getInputValue);
+createDivs(gridSize);
